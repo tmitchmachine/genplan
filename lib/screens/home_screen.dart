bc1e46a48d898502a95f18b641da8d15620ed0b2
hammadalive7 <hammadali1810@gmail.com>
2024-08-21 18:19:25 +0500

    added Firebase, Email/Password Sign in, and Email/Password Sign up

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
 class HomeScreen extends StatelessWidget {
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Welcome to My App',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
-            Text('This is a basic home screen example.'),
-            SizedBox(height: 32),
+            const SizedBox(height: 16),
+            const Text('This is a basic home screen example.'),
+            const SizedBox(height: 32),
             ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 // Button press logic here
-                print('Button pressed!');
+                await auth.signOut();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
               },
-              child: Text('Click Me'),
+              child: const Text('Log Out'),
             ),
           ],
         ),

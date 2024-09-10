985612a5f6ffcacdb93744aeaaecbae90fff51da
Travis Mitchell <tmitch5151@gmail.com>
2024-09-10 09:04:14 -0700

    Added When Thumb Selected UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:genplan/screens/plan.dart'; // Ensure this is correct
 import 'package:genplan/screens/menu.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isThumbsUpSelected = false;
+  bool _isThumbsDownSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +28,7 @@ class HomePage extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) =>
-                    MenuPage(auth: auth), // Pass the auth instance here
+                builder: (context) => MenuPage(auth: widget.auth),
               ),
             );
           },
@@ -38,9 +45,7 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-            top: MediaQuery.of(context).size.height * 0.5 -
-                20 -
-                160, // Move text 2 inches higher
+            top: MediaQuery.of(context).size.height * 0.5 - 20 - 160,
             left: 0,
             right: 0,
             child: Center(
@@ -51,20 +56,51 @@ class HomePage extends StatelessWidget {
             ),
           ),
           Positioned(
-            bottom: 80, // Move button up by 0.5 inches
+            bottom: 80,
             left: 0,
             right: 0,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment
-                  .spaceBetween, // Space between thumbs up and thumbs down
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // Thumbs up icon
+                // Thumbs up icon with a nice UI
                 Padding(
                   padding: const EdgeInsets.only(left: 16.0),
-                  child: Image.network(
-                    'https://firebasestorage.googleapis.com/v0/b/genplan-007.appspot.com/o/thumb_up.png?alt=media&token=46385d68-85ee-4d87-87f5-27ac207efa7d',
-                    width: 24,
-                    height: 24,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isThumbsUpSelected = !_isThumbsUpSelected;
+                        _isThumbsDownSelected =
+                            false; // Reset thumbs down when thumbs up is selected
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      width: _isThumbsUpSelected ? 48 : 40,
+                      height: _isThumbsUpSelected ? 48 : 40,
+                      decoration: BoxDecoration(
+                        color: _isThumbsUpSelected
+                            ? Colors.black
+                            : Colors.transparent, // Transparent when unselected
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          if (_isThumbsUpSelected)
+                            BoxShadow(
+                              color: Colors.black
+                                  .withOpacity(0.3), // Lighter shadow
+                              spreadRadius: 3,
+                              blurRadius: 8,
+                            ),
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.thumb_up_alt_rounded,
+                        color: _isThumbsUpSelected
+                            ? Colors.white
+                            : Colors
+                                .grey.shade400, // Lighter gray when unselected
+                        size: _isThumbsUpSelected ? 28 : 24,
+                      ),
+                    ),
                   ),
                 ),
                 Container(
@@ -81,36 +117,65 @@ class HomePage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) =>
-                              PlanPage(), // Navigate to PlanPage
+                          builder: (context) => PlanPage(),
                         ),
                       );
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          Colors.transparent, // Set background to transparent
-                      foregroundColor: Colors.white, // Text color
-                      elevation: 0, // Remove shadow
-                      minimumSize: Size(200, 50), // Button size
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.white,
+                      elevation: 0,
+                      minimumSize: Size(200, 50),
                       padding: EdgeInsets.symmetric(
                         vertical: 16,
                         horizontal: 32,
-                      ), // Button padding
+                      ),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            8.0), // Match the container's border radius
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
                     child: const Text('Generate Plan'),
                   ),
                 ),
-                // Thumbs down icon
+                // Thumbs down icon with a nice UI
                 Padding(
                   padding: const EdgeInsets.only(right: 16.0),
-                  child: Image.network(
-                    'https://firebasestorage.googleapis.com/v0/b/genplan-007.appspot.com/o/thumb_down.png?alt=media&token=1439cad8-0f8b-48c9-8275-f6b799196cec',
-                    width: 24,
-                    height: 24,
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isThumbsDownSelected = !_isThumbsDownSelected;
+                        _isThumbsUpSelected =
+                            false; // Reset thumbs up when thumbs down is selected
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      width: _isThumbsDownSelected ? 48 : 40,
+                      height: _isThumbsDownSelected ? 48 : 40,
+                      decoration: BoxDecoration(
+                        color: _isThumbsDownSelected
+                            ? Colors.black
+                            : Colors.transparent, // Transparent when unselected
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          if (_isThumbsDownSelected)
+                            BoxShadow(
+                              color: Colors.black
+                                  .withOpacity(0.3), // Lighter shadow
+                              spreadRadius: 3,
+                              blurRadius: 8,
+                            ),
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.thumb_down_alt_rounded,
+                        color: _isThumbsDownSelected
+                            ? Colors.white
+                            : Colors
+                                .grey.shade400, // Lighter gray when unselected
+                        size: _isThumbsDownSelected ? 28 : 24,
+                      ),
+                    ),
                   ),
                 ),
               ],
